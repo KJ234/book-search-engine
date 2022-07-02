6585a170212532207a7c31fb6503dd753ab7ec54
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-07-02 22:56:13 +0100

    apollo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Books {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Books]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: [String]
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
